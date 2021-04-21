82156b78432142497dceac871fbff3f98755f463
mertdogan <mertcandogan@windowslive.com>
2021-04-22 01:23:14 +0300

    headless

--- NOVAA/src/test/java/tests/BaseTest.java
+++ NOVAA/src/test/java/tests/BaseTest.java
@@ -5,10 +5,12 @@ package tests;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 public class BaseTest {
@@ -20,9 +22,12 @@ public class BaseTest {
     public void setup () throws InterruptedException {
        // System.setProperty("webdriver.chrome.driver", "\\chromedriver\\chromedriver.exe");
         System.setProperty("webdriver.gecko.driver", "\\geckodriver.exe");
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        Thread.sleep(1500);
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        //this is for headless. if you need browser you should comment below line after disable comment driver.manage line.
+        firefoxOptions.setHeadless(true);
+        driver = new FirefoxDriver(firefoxOptions);
+        //driver.manage().window().maximize();
+        //Thread.sleep(1500);
         wait = new WebDriverWait(driver, 30);
 
 
